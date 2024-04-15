d3459c2d148b0315786c2f645da734fbafc01438
Chaitanya Ashok Raj <Chaitanya.Raj@Geminisolutions.com>
2024-04-15 06:50:11 +0530

    ''

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()
 def get_vectorstore_from_text(repo_name):
-     file = f"./repo_text/{repo_name}_analysis.txt"
-     # document = TextLoader(file).load()
-     document = DirectoryLoader("./repo_text", glob=f"{repo_name}_analysis.txt", loader_cls=TextLoader).load()
+     file = f"./{repo_name}_analysis.txt"
+     document = TextLoader('Push-The-Box_analysis.txt').load()
+     # document = DirectoryLoader("./repo_text", glob=f"{repo_name}_analysis.txt", loader_cls=TextLoader).load()
      # document = DirectoryLoader(f"./{repo_name}/cloned_repo", glob="**/*.py", loader_cls=PythonLoader).load()
      os.remove(f"./repo_text/{repo_name}_analysis.txt")
      # loader = WebBaseLoader(url)
@@ -40,12 +40,12 @@ def get_vectorstore_from_text(repo_name):
      # return st.session_state.vector_store
 
 def get_context_retriever_chain(vector_store):
-     # llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-     llm = huggingface_hub.HuggingFaceHub(
-          huggingfacehub_api_token=os.getenv('HF_API_KEY'),
-          task='text-generation'
+     llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+     # llm = huggingface_hub.HuggingFaceHub(
+          # huggingfacehub_api_token=os.getenv('HF_API_KEY'),
+          # task='text-generation'
           # repo_id='CohereForAI/c4ai-command-r-plus'
-     )
+     # )
      retriever = vector_store.as_retriever()
      
      prompt = ChatPromptTemplate.from_messages([
@@ -59,12 +59,12 @@ def get_context_retriever_chain(vector_store):
 
 def get_conversational_rag_chain(retriever_chain): 
     
-     # llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-     llm = huggingface_hub.HuggingFaceHub(
-          huggingfacehub_api_token=os.getenv('HF_API_KEY'),
-          task='text-generation'
+     llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+     # llm = huggingface_hub.HuggingFaceHub(
+          # huggingfacehub_api_token=os.getenv('HF_API_KEY'),
+          # task='text-generation'
           # repo_id='CohereForAI/c4ai-command-r-plus'
-     )
+     # )
      prompt = ChatPromptTemplate.from_messages([
           ("system", "Answer the user's questions based on the below context:\n\n{context}"),
           MessagesPlaceholder(variable_name="chat_history"),
@@ -163,7 +163,7 @@ def save_repo_analysis(repo_url, repo_name):
           analysis_text += f"Function names: {', '.join(all_function_names)}\n"
 
           # Write analysis text to file
-          output_file = f"./repo_text/{repo_name}_analysis.txt"
+          output_file = f"{repo_name}_analysis.txt"
           with open(output_file, "w", encoding="utf-8") as f:
                f.write(analysis_text)
 
