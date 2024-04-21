b1002eb87a010fd0bea0493dddb9c480e1e038f8
Chaitanya Ashok Raj <Chaitanya.Raj@Geminisolutions.com>
2024-04-22 02:25:55 +0530

    loader update

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ def main():
           user_query = st.chat_input("Type your message here...")
           if user_query is not None and user_query != "":
                response = get_response(user_query)
-               st.write(st.session_state.chat_history)
+               # st.write(st.session_state.chat_history)
                st.session_state.chat_history.append(HumanMessage(content=user_query))
                st.session_state.chat_history.append(AIMessage(content=response['answer']))
                # st.write(st.session_state)
