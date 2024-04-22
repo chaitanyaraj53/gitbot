228808c415244c5f28dede6c24bb571e94136f6a
Chaitanya Ashok Raj <Chaitanya.Raj@Geminisolutions.com>
2024-04-22 13:53:42 +0530

    loader update

--- app.py
+++ app.py
@@ -114,7 +114,10 @@ def get_context_retriever_chain(vector_store):
           # task='text-generation'
           # repo_id='CohereForAI/c4ai-command-r-plus'
      # )
-     retriever = vector_store.as_retriever()
+     retriever = vector_store.as_retriever(
+          search_type="mmr",
+          search_kwargs={'k': 6, 'lambda_mult': 0.25}
+     )
      
      prompt = ChatPromptTemplate.from_messages([
           MessagesPlaceholder(variable_name="chat_history"),
